925379a56745246fae007292c7338bf45106e01c
Sunny420x.com <costerraid@gmail.com>
2024-01-28 12:48:52 +0700

    Navigate using arrow key.

--- summer_gallery.js
+++ summer_gallery.js
@@ -1,5 +1,5 @@
 const summer_gallery = document.getElementsByClassName("summer_gallery")
-var image_id = 0
+var current_image = ""
 document.getElementsByClassName("backdrop")[0].style.display = "none"
 
 for(i = 0; i < summer_gallery.length; i++) {
@@ -15,8 +15,28 @@ function show_image(id) {
     if(document.getElementById(id).className != "active") {
         document.getElementById(id).setAttribute("class", "active")
         document.getElementsByClassName("backdrop")[0].style.display = "block"
+        current_image = id
     } else {
         document.getElementById(id).setAttribute("class", "")
         document.getElementsByClassName("backdrop")[0].style.display = "none"
+        current_image = ""
     }
 }
+
+document.addEventListener('keydown', (e) => {
+    console.log(e.key)
+    if(current_image != undefined) {
+        if(e.key === "ArrowLeft") {
+            document.getElementById(current_image).setAttribute("class", "")
+            document.getElementsByClassName("backdrop")[0].style.display = "none"
+            _id = current_image.split("_")
+            show_image("pic_"+_id[1]+"_"+(parseInt(_id[2])-1))
+        }
+        if(e.key === "ArrowRight") {
+            document.getElementById(current_image).setAttribute("class", "")
+            document.getElementsByClassName("backdrop")[0].style.display = "none"
+            _id = current_image.split("_")
+            show_image("pic_"+_id[1]+"_"+(parseInt(_id[2])+1))
+        }
+    }
+})
